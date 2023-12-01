893e9d5eda0937d134718d062b6d57dfd3f6803a
Voegl <Bananenpuree2@gmail.com>
2023-12-01 18:09:31 +0100

    ,

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Drivercontrolled", group = "")
@@ -13,6 +14,8 @@ public class Drive extends LinearOpMode {
     private DcMotor motorRightFront;
     private DcMotor motorLeftBack;
     private DcMotor motorRightBack;
+    private DcMotor motorIntake;
+    private Servo Vliegtuig;
 
     @Override
     public void runOpMode () {
@@ -23,9 +26,13 @@ public class Drive extends LinearOpMode {
         motorLeftBack = hardwareMap.get(DcMotor.class, "linksachter");
         motorRightBack = hardwareMap.get(DcMotor.class, "rechtsachter");
         motorRightFront = hardwareMap.get(DcMotor.class, "rechtsvoor");
+        motorIntake = hardwareMap.get(DcMotor.class, "intake");
+        Vliegtuig = hardwareMap.get(Servo.class, "vliegtuig");
         double x;
         double y;
+        double z;
         double TurnRate;
+        motorIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
 
 
@@ -33,14 +40,20 @@ public class Drive extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
-            x = 0.5*gamepad1.left_stick_x;
+            x = -0.5*gamepad1.left_stick_x;
             y = 0.5*gamepad1.left_stick_y;
             TurnRate = 0.5*(gamepad1.right_trigger - gamepad1.left_trigger);
+            z = 0.5*gamepad2.left_stick_y;
+            if (gamepad2.a){
+                Vliegtuig.setPosition(1);
+            }
 
             motorRightBack.setPower(x - y - TurnRate);
             motorLeftFront.setPower(-x + y - TurnRate);
             motorLeftBack.setPower(x + y - TurnRate);
             motorRightFront.setPower(x + y + TurnRate);
+            motorIntake.setPower(z);
+
         }
     }
 }
